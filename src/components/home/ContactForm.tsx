cf6822f250f46509a8b5aa8b4be25ea1d03eaefe
Yaser Syafa <89734955+yasersyafa@users.noreply.github.com>
2025-08-19 16:49:14 +0700

    Update ContactForm.tsx

--- src/components/home/ContactForm.tsx
+++ src/components/home/ContactForm.tsx
@@ -126,6 +126,7 @@ export default function ContactForm({ onSuccess }: ContactFormProps) {
                   <SelectItem value="2022">2022</SelectItem>
                   <SelectItem value="2023">2023</SelectItem>
                   <SelectItem value="2024">2024</SelectItem>
+                  <SelectItem value="2025">2025</SelectItem>
                 </SelectContent>
               </Select>
               <FormMessage />

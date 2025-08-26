56d4501262faf4780147945a7cf10c755e478ffb
Yaser Syafa <syafayaser@gmail.com>
2025-08-26 16:05:15 +0700

    feat(hero-section): update button text and improve layout

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,27 +1,51 @@
-'use client'
+import { Button } from "@/components/ui/button";
+import Star9 from "@/components/stars/s9";
 
-import { Button } from "@/components/ui/button"
-import Star9 from "@/components/stars/s9"
+const handleCTAClick = () => {
+  // open new tab and go to gform
+  window.open(
+    "https://intip.in/PeminjamanAlatDanPerizinanPenggunaanLabGT",
+    "_blank"
+  );
+};
 
 export default function HeroSection() {
-    return (
-        <section className="container px-5 sm:px-0 mx-auto min-h-screen flex flex-col items-center justify-center space-y-20">
-            {/* title */}
-            <div className="flex flex-col space-y-5 justify-center items-center">
-                <h1 className="font-extrabold text-4xl sm:text-5xl text-center text-pretty leading-snug select-text">
-                    <span className="relative bg-main/50 border-2 border-main/40 rounded-base px-2">
-                        <Star9 pathClassName="bg-main" color="#FF98AD" size={36} className="absolute sm:block hidden md:-bottom-4 md:-right-5 -bottom-2.5 -right-2.5 stroke-5 dark:stroke-3.5 stroke-black dark:stroke-black/70" />
-                        <Star9 pathClassName="bg-main" color="#FF98AD" size={36} className="absolute sm:block hidden md:-top-4 md:-left-5 -top-2.5 -left-2.5 stroke-5 dark:stroke-3.5 stroke-black dark:stroke-black/70" />
-                        Game Technology
-                    </span>
-                    <br />
-                    Laboratory Assistant.
-                </h1>
-                <p className="font-base sm:text-lg md:text-xl max-w-3xl sm:max-w-4xl text-center">A dedicated group of students who strive to learning, collaboration, and creativity — where code meets art, game design comes to life, and the magic of innovation is brewed daily.</p>
-            </div>
-            <Button size={'cta'} className="text-xl font-bold cursor-pointer">
-                Get To Know Us
-            </Button>
-        </section>
-    )
+  return (
+    <section className="container px-5 sm:px-0 mx-auto min-h-screen flex flex-col items-center justify-center space-y-20">
+      {/* title */}
+      <div className="flex flex-col space-y-5 justify-center items-center">
+        <h1 className="font-extrabold text-4xl sm:text-5xl text-center text-pretty leading-snug select-text">
+          <span className="relative bg-main/50 border-2 border-main/40 rounded-base px-2">
+            <Star9
+              pathClassName="bg-main"
+              color="#FF98AD"
+              size={36}
+              className="absolute sm:block hidden md:-bottom-4 md:-right-5 -bottom-2.5 -right-2.5 stroke-5 dark:stroke-3.5 stroke-black dark:stroke-black/70"
+            />
+            <Star9
+              pathClassName="bg-main"
+              color="#FF98AD"
+              size={36}
+              className="absolute sm:block hidden md:-top-4 md:-left-5 -top-2.5 -left-2.5 stroke-5 dark:stroke-3.5 stroke-black dark:stroke-black/70"
+            />
+            Game Technology
+          </span>
+          <br />
+          Laboratory Assistant.
+        </h1>
+        <p className="font-base sm:text-lg md:text-xl max-w-3xl sm:max-w-4xl text-center">
+          A dedicated group of students who strive to learning, collaboration,
+          and creativity — where code meets art, game design comes to life, and
+          the magic of innovation is brewed daily.
+        </p>
+      </div>
+      <Button
+        size={"cta"}
+        className="text-xl font-bold cursor-pointer"
+        onClick={handleCTAClick}
+      >
+        Request Lab Access
+      </Button>
+    </section>
+  );
 }
